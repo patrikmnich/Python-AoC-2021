c6344ebc34d56c537c5d21da505d7365f4ea4c02
Patrik Mních <mnich.patrik@gmail.com>
2021-12-06 19:31:55 +0100

    Initial commit

---
+++ src/Day01.py
@@ -0,0 +1,12 @@
+def part1(inputfile):
+    with open(inputfile) as f:
+        inputfile = f.readlines()
+
+def part2(inputfile):
+    with open(inputfile) as f:
+        inputfile = f.readlines()
+
+inputfile = "Day01.txt"
+
+print(part1(inputfile))
+print(part2(inputfile))
