3b267149dfb01446705647fa93031f3e624804df
Patrik Mních <mnich.patrik@gmail.com>
2021-12-07 23:15:54 +0100

    Day 3 solution
    note to self - don't modify the variable you are looping through

--- src/Day03.py
+++ src/Day03.py
@@ -33,43 +33,58 @@ def part2(inputfile):
         file_content = [line.strip() for line in file_content]
 
     
-
     oxygen = file_content[:]
     co2 = file_content[:]
 
-    while(len(oxygen) > 1):
-        zeros = [0 for i in file_content[0]]
-        ones = [0 for i in file_content[0]]
-        for line in oxygen:
-            for i in range(0,len(line)):
-                if line[i] == "0":
-                    zeros[i] += 1
-                else:
-                    ones[i] += 1
+    bitlength = len(file_content[0])
 
-        for i in range(len(zeros)):
-            most_common = "1" if ones[i] > zeros[i] else "0"
+    done = False
+    o2_done = False
+    co2_done = False
+
+    while not done:
+        for i in range(bitlength):
+            
+            if not o2_done:
+                ones = 0
+                zeros = 0
+                for line in oxygen:
+                    if line[i] == "0":
+                        zeros += 1
+                    else:
+                        ones += 1
+        
+                most_common = "1" if ones >= zeros else "0"
     
-            for line in oxygen:
-                if line[i] != most_common:
-                    oxygen.remove(line)
+            
+                for line in oxygen[:]:
+                    if line[i] == most_common:
+                        oxygen.remove(line)
+                    if len(oxygen) == 1:
+                        o2_done = True
+
+            if not co2_done:
+
+                ones = 0
+                zeros = 0
+
+                for line in co2:
+                    if line[i] == "0":
+                        zeros += 1
+                    else:
+                        ones += 1
+        
+                least_common = "0" if zeros <= ones else "1"
+        
+                for line in co2[:]:
+                    if line[i] == least_common:
+                        co2.remove(line)
+                    if len(co2) == 1:
+                        co2_done = True
+        
+        if co2_done and o2_done:
+            done = True
     
-    while(len(co2) > 1):
-        zeros = [0 for i in file_content[0]]
-        ones = [0 for i in file_content[0]]
-        for line in co2:
-            for i in range(0,len(line)):
-                if line[i] == "0":
-                    zeros[i] += 1
-                else:
-                    ones[i] += 1
-
-        for i in range(len(zeros)):
-            least_common = "1" if ones[i] <= zeros[i] else "0"
-            for line in co2:
-                if line[i] == most_common:
-                    co2.remove(line)
-
     print(oxygen,co2)
     
     return int(oxygen[0],2)*int(co2[0],2)
